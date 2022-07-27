03a82da7d91fa3c1a84a3c57d3ae4c795577463c
Madhusudan Sharma <sharmamadhusudan@hotmail.com>
2022-07-27 15:19:29 -0500

    Add project files.

---
+++ CustomerCRM.Infrastructure/Data/CustomerCrmDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CustomerCRM.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerCRM.Infrastructure.Data
+{
+    public class CustomerCrmDbContext:DbContext
+    {
+        public CustomerCrmDbContext(DbContextOptions<CustomerCrmDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Region { get; set; }
+        public DbSet<Shipper> Shipper { get; set; }
+    }
+}
